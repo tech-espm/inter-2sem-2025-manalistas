966626f24b0813d10cb79edfd66de24079f0ab9b
Ihago Nunes <ihago2nunes@gmail.com>
2025-10-11 19:56:13 -0300

    Criação de Rotas e Atualização da Navbar
    Adicionado rotas para Adestramento, Banho e Tosa, Clínica Veterinária, Daycare, Hotel, and Táxi Dog no arquivo routes/index.js. Atualização da barra de navegação do arquivo barra.ejs para incluir essas novas rotas.

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,55 @@ router.get("/", wrap(async (req, res) => {
 	res.render("index/index");
 }));
 
+router.get("/", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Home"
+	};
+	res.render("index/home", opcoes);
+}));
+
 router.get("/sobre", wrap(async (req, res) => {
 	let opcoes = {
-		titulo: "Sobre"
+		titulo: "Sobre Nós"
 	};
-
 	res.render("index/sobre", opcoes);
 }));
+
+router.get("/adestramento", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Adestramento"
+	};
+	res.render("index/adestramento", opcoes);
+}));
+
+router.get("/banhotosa", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Banho e Tosa"
+	};
+	res.render("index/banhotosa", opcoes);
+}));
+
+router.get("/clinica", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Clínica Veterinária"
+	};
+	res.render("index/clinica", opcoes);
+}));
+
+router.get("/daycare", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Daycare"
+	};
+	res.render("index/daycare", opcoes);
+}));
+
+router.get("/hotel", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Hotel"
+	};
+	res.render("index/hotel", opcoes);
+}));
+
 
 router.get("/produtos", wrap(async (req, res) => {
 	let produtoA = {
@@ -43,5 +85,21 @@ router.get("/produtos", wrap(async (req, res) => {
 
 	res.render("index/produtos", opcoes);
 }));
+
+router.get("/sobre", wrap(async (req, res) => {
+	let opcoes = {
+		titulo: "Sobre Nós"
+	};
+
+	res.render("index/sobre", opcoes);
+}));
+
+router.get("/taxidog", wrap(async(req, res) => {
+	let opcoes = {
+		titulo: "Táxi Dog"
+	};
+	
+	res.render("index/taxidog", opcoes)
+}))
 
 module.exports = router;
