1963e8df9112b40b2a9e14d72b361b1e432f5ee4
bevolpi <betinavolpi2006@gmail.com>
2025-11-09 17:27:30 -0300

    imagens
    atualizadinhas

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,16 @@
+const elements = document.querySelectorAll('.animate-on-scroll');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    });
+
+    elements.forEach(el => observer.observe(el));
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const inner = document.querySelector('#depoimentosCarousel .carousel-inner');
   
